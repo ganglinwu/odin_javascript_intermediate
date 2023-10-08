7c8376dbfab3dc59216c70ae49566dd92075c8d3
ganglinwu <ganglin.wu@gmail.com>
2023-10-08 15:54:13 +0800

    to prepare for _miniMax, added argument to functions
    - _updateValues now takes in board as argument
    - _updateValues also returns a scoreArray now
    
    - _boardValueSum also now takes in board as an additional argument
    
    - _determine now takes in scoreArray and board as argument

--- 04-tic-tac-toe/script.js
+++ 04-tic-tac-toe/script.js
@@ -28,7 +28,7 @@ const tictactoe = (() => {
     let _gBoardValue = new Array(9).fill(null);
 
     // initialize win condition values
-    let _r1 , _r2, _r3, _c1, _c2, _c3, _d1, _d2, _rowColumnDiagonalArr;
+    let _rowColumnDiagonalScores;
     
     // private method to create Player objects
     const _Player = (name, value) => {
@@ -55,15 +55,17 @@ const tictactoe = (() => {
                 // mark player1's move on html page
                 // log player1's value into gBoardValues Array
 
-                _updateValues();
-                // update all Values, row sum, column sum, diagonal sum
+                _rowColumnDiagonalScores = _updateValues(_gBoardValue);
+                // update array which holds: row sum, column sum, diagonal sum
+                
                 // and check if win draw or continue
                 // (since losing requires com to make a move we need not check for loss)
-                
-                if (_determine() === 1) {
+                let _result = _determine(_rowColumnDiagonalScores, _gBoardValue);
+
+                if (_result === 1) {
                     _resultSpan.innerText = `${_p1.getName()} wins!`;
                     _gameOver = true;
-                } else if (_determine() === 2) {
+                } else if (_result === 2) {
                     _resultSpan.innerText = 'It\'s a draw!';
                     _gameOver= true;
                 }
@@ -84,12 +86,12 @@ const tictactoe = (() => {
                     // mark computer's move on html page
                     // log computer's value into gBoardValues Array
                     
-                    _updateValues();
+                    _rowColumnDiagonalScores = _updateValues(_gBoardValue);
                     // update row, column diagonal sum
                     // check if computer win
                     // (we need not check for player win or draw!)
                     
-                    if (_determine() === -1) {
+                    if (_determine(_rowColumnDiagonalScores, _gBoardValue) === -1) {
                         _resultSpan.innerText = 'Computer wins!';
                         _gameOver = true;
                     }
@@ -124,37 +126,39 @@ const tictactoe = (() => {
         Array.from(_tttBoxNodeList).forEach(box=> box.innerText = '');
         _resultSpan.innerText = '';
         _gameOver = false;
-        _updateValues();
+        _rowColumnDiagonalScores = _updateValues(_gBoardValue);
         _playerMoveCount = 0;
         _comMoveCount = 0;
     }
 
 
-    // helper function to check sum of values in array
-    function _gBoardValueSum(a,b,c) {
-        let _sum = _gBoardValue[a]+_gBoardValue[b]+_gBoardValue[c];
+    // helper function to check sum of values in board array
+    function _boardValueSum(board,a,b,c) {
+        let _sum = board[a]+board[b]+board[c];
         if (_sum === 3 || _sum === -3) {
             return _sum*10;
         } else return _sum;
     }
 
     // helper function to update win condition values
-    function _updateValues() {
+    function _updateValues(board) {
         /* variables that check horizontal win condition*/
-        _r1 = _gBoardValueSum(0,1,2);
-        _r2 = _gBoardValueSum(3,4,5);
-        _r3 = _gBoardValueSum(6,7,8);
+        let _r1 = _boardValueSum(board,0,1,2);
+        let _r2 = _boardValueSum(board,3,4,5);
+        let _r3 = _boardValueSum(board,6,7,8);
     
         /* variables that check vertical win condition*/
-        _c1 = _gBoardValueSum(0,3,6);
-        _c2 = _gBoardValueSum(1,4,7);
-        _c3 = _gBoardValueSum(2,5,8);
+        let _c1 = _boardValueSum(board,0,3,6);
+        let _c2 = _boardValueSum(board,1,4,7);
+        let _c3 = _boardValueSum(board,2,5,8);
     
         /* variables that check diagnonal win condition*/
-        _d1 = _gBoardValueSum(0,4,8);
-        _d2 = _gBoardValueSum(6,4,2);
+        let _d1 = _boardValueSum(board,0,4,8);
+        let _d2 = _boardValueSum(board,6,4,2);
     
-        _rowColumnDiagonalArr = [_r1, _r2, _r3, _c1, _c2, _c3, _d1, _d2];
+        let arr = [_r1, _r2, _r3, _c1, _c2, _c3, _d1, _d2];
+
+        return arr;
     }
 
 
@@ -177,12 +181,12 @@ const tictactoe = (() => {
     }
 
     // determine winner
-    function _determine() {
-        if (_rowColumnDiagonalArr.includes(30)) {
+    function _determine(scoresArray, board) {
+        if (scoresArray.includes(30)) {
             return 1; // player win
-        } else if (_rowColumnDiagonalArr.includes(-30)) {
+        } else if (scoresArray.includes(-30)) {
             return -1; // computer win 
-        } else if (_gBoardValue.some(element => element===null)) {
+        } else if (board.some(element => element===null)) {
             return 0; // there are still empty boxes, game has not ended
         } else {
             return 2; // game ended, no more boxes to click
