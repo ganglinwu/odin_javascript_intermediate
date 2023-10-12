a44a083d7a64c6ecad970732a3a7bfcd0deaed24
ganglinwu <ganglin.wu@gmail.com>
2023-10-12 12:51:23 +0800

    Output management
    - how to output multiple js files
    - use html-webpack-plugin to handle multiple js src files in index.html
    - cleaning up /dist folder

--- 06-webpack-demo/src/index.js
+++ 06-webpack-demo/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import myName from './myName';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    // Lodash?? is required for this line to work
     element.textContent = myName('Sven');
+    element.classList.add('hello');
+
+    btn.innerText = 'Click me and open the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
--- 06-webpack-demo/webpack.config.js
+++ 06-webpack-demo/webpack.config.js
@@ -1,8 +1,20 @@
-module.exports = {  
-    mode: 'development',  
-    entry: './src/index.js',  
-    output: {  
-      filename: 'main.js',  
-      publicPath: 'dist'  
-    }  
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+ module.exports = {
+   entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Output Management',
+      }),
+    ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
   };
